1791af6426339f08bb849da0cb908a9d255660e4
Javier Bullrich <javierbullrich@gmail.com>
2018-08-27 20:15:18 -0300

    Finished Lecture 55

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,5 +1,13 @@
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./service_account.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-4eace.firebaseio.com"
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
