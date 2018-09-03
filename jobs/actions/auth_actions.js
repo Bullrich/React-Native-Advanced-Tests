a35a6cc36093d476f8c02b85131c3caec5c6204c
Javier Bullrich <javierbullrich@gmail.com>
2018-09-02 23:30:01 -0300

    Finished Lecture 99

--- jobs/actions/auth_actions.js
+++ jobs/actions/auth_actions.js
@@ -8,6 +8,11 @@ import {
 // AsyncStorage.setItem('fb_token', value);
 // AsyncStorage.getItem('fb_token');
 
-export const facebookLogin = () => {
-
+export const facebookLogin = () => async dispatch => {
+  let token = await AsyncStorage.getItem('fb_token');
+  if (token) {
+    // Dispatch action
+  } else {
+    // Start up FB login process
+  }
 };
