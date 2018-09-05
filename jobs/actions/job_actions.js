709b2d1987e2b64c3c2ac2ea2632faf64c40cbe9
Javier Bullrich <javierbullrich@gmail.com>
2018-09-05 01:33:32 -0300

    Finished Lecture 128

--- jobs/actions/job_actions.js
+++ jobs/actions/job_actions.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 import reverseGeocode from 'latlng-to-zip';
 import qs from 'qs';
 import {FETCH_JOBS} from "./types";
@@ -20,13 +20,14 @@ const buildJobsUrl = (zip) => {
   return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = (region) => async dispatch => {
+export const fetchJobs = (region, callback) => async dispatch => {
   try {
     let zip = await reverseGeocode(region);
     const url = buildJobsUrl(zip);
     console.log(url);
     // let {data} = await axios.get(url);
     dispatch({type: FETCH_JOBS, payload: JOB_DATA});
+    callback();
   }
   catch (e) {
     console.error(e);
